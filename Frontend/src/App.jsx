5670b0b0c7c2a7d14c037d0fff3e477f8cbe7d3f
MehakSingh123 <jibanny648@gmail.com>
2024-10-09 16:49:00 +0530

    change the speech settings

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,15 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ColorSchemesExample from "./Navbar/Navbar";
 import FilterComponent from "./Filters/Filter";
 import SpeechToText from './speechToText/speechToText';
+import UserTable from './components/UserTable';
+import JobNavbar from './Navbar/Navbar';
+import Login from './Login/Login'
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   const [showUserTable, setShowUserTable] = useState(false);  // Add state to control UserTable visibility
 
   return (
     <>
-      {/* Fixed Navbar - Stays at the top but doesn't overlap */}
-      <div style={{ position: "fixed", top: 0, width: "100%", zIndex: 1000 }}>
-        <JobNavbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={
+      <div>{/* Fixed Navbar - Stays at the top but doesn't overlap */}
+      <div style={{ position: "fixed", top: 0, width: "100%" }}>
+        <div style={{zIndex:1000}}><JobNavbar /></div>
         <FilterComponent />
       </div>
 
@@ -19,7 +26,17 @@ function App() {
       <div style={{ paddingTop: '280px', marginLeft: "400px" }}> {/* Add top padding equal to navbar height */}
           
       </div>
-      
+          
+          </div>}
+          />
+          <Route path='/users' element={
+            <UserTable/>
+          }/>
+          <Route path='/login' element={
+            <Login/>
+          }/>
+        </Routes>
+      </Router>
       
     </>
   );

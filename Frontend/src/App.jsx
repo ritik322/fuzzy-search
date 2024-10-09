da5ec90a8aaea229798d869e07522da83bac389b
MehakSingh123 <jibanny648@gmail.com>
2024-10-09 10:58:26 +0530

    added speech recognition

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import JobNavbar  from "./Navbar/Navbar";
 import FilterComponent from "./Filters/Filter";
+import SpeechToText from './speechToText/speechToText';
 
 function App() {
   return (
     <>
       {/* Fixed Navbar - Stays at the top but doesn't overlap */}
-      <div style={{ position: "fixed", top: 0, width: "100%", zIndex: 1000 }}>
+      <div style={{ top: 0, width: "100%", zIndex: 1000 }}>
         <JobNavbar />
         <FilterComponent />
       </div>
 
       {/* Content Below the Navbar */}
-      <div style={{ paddingTop: '280px', marginLeft: "400px" }}> {/* Add top padding equal to navbar height */}
-          
+      <div style={{ paddingTop: '280px', zIndex: 1000, marginLeft: "400px" }}> {/* Add top padding equal to navbar height */}
+      <SpeechToText/>
       </div>
+      
       
     </>
   );
